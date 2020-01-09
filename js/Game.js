1d9e37451fd2b8e2d1b7532eeb20b1ace6785758
Garrett Trott <garretttrott@gmail.com>
2020-01-09 00:57:58 -0800

    adds createPhrases & getRandomPhrases methods

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,32 @@
 class Game {
   constructor() {
     this.missed = 0;
-    this.phrases = [];
+    this.phrases = this.createPhrases();
     this.activePhrase = null;
+  }
+
+  /**
+   * Creates phrases for use in game
+   * @return {array} An array of phrases tha could be used in the game
+   */
+
+  createPhrases() {
+    const phrases = [
+      new Phrase("A penny saved is a penny earned"),
+      new Phrase("A dime a dozen"),
+      new Phrase("Beating around the bush"),
+      new Phrase("Once in a blue moon"),
+      new Phrase("Speak of the devil")
+    ];
+    return phrases;
+  }
+
+  /**
+   * Selects a random phrase from phrases property
+   * @return {object} Phrase object to be used
+   */
+
+  getRandomPhrase() {
+    return this.phrases[Math.floor(Math.random() * this.phrases.length)];
   }
 }
