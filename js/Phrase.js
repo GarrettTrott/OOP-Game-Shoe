724bf0895b98620b551f2be96e0a8d658d8119a4
Garrett Trott <garretttrott@gmail.com>
2020-01-11 23:50:13 -0800

    adds addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,24 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+
+  /**
+   * Display phrase on game board
+   */
+
+  addPhraseToDisplay() {
+    const phraseUl = document.getElementById("phrase");
+    const phraseArray = this.phrase.split("");
+
+    phraseArray.forEach(letter => {
+      const li = document.createElement("LI");
+      li.textContent = letter;
+      if (letter === " ") {
+        li.className = "space";
+      } else {
+        li.className = `hide letter ${letter}`;
+      }
+      phraseUl.appendChild(li);
+    });
+  }
 }
